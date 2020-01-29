664a48936c103781e71d8ad56457fb260127c72b
Matthew Thompson <Matthew.Thompson@nottingham.ac.uk>
2020-01-29 14:39:02 +0000

    CTextileBraidCurved added

---
+++ Core/TextileBraidCurved.cpp
@@ -0,0 +1,17 @@
+#include "TextileBraidCurved.h"
+#include "PrecompiledHeaders.h"
+#include "SectionEllipse.h"
+#include "SectionRotated.h"
+#include "SectionPolygon.h"
+#include "SectionLenticular.h"
+
+using namespace TexGen;
+
+CTextileBraidCurved::CTextileBraidCurved()
+{
+}
+
+
+CTextileBraidCurved::~CTextileBraidCurved()
+{
+}
---
+++ Core/TextileBraidCurved.h
@@ -0,0 +1,17 @@
+#include "TextileBraid.h"
+#include "DomainPlanes.h"
+#include "mymath.h"
+
+#pragma once
+namespace TexGen
+{
+	using namespace std;
+	class CLASS_DECLSPEC CTextileBraidCurved: public CTextileBraid
+	{
+		public:
+			CTextileBraidCurved();
+			~CTextileBraidCurved();
+	
+	}
+};
+
